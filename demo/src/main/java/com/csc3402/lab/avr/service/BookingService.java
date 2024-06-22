2a420df6289879a895065b3f86bad389cab55a90
rizkyagungnurbaskoro <rizky.nurbaskoro@gmail.com>
2024-06-22 23:40:41 +0800

    Rizky Changes

--- demo/src/main/java/com/csc3402/lab/avr/service/BookingService.java
+++
@@ -1,9 +0,0 @@
-package com.csc3402.lab.avr.service;
-
-import com.csc3402.lab.avr.model.Booking;
-
-import java.util.List;
-
-public interface BookingService {
-    List<Booking> listAllBookings();
-}
--- demo/src/main/java/com/csc3402/lab/avr/service/RoomService.java
+++
@@ -1,9 +0,0 @@
-package com.csc3402.lab.avr.service;
-
-import com.csc3402.lab.avr.model.Room;
-
-import java.util.List;
-
-public interface RoomService {
-    List<Room> listAllRooms();
-}
